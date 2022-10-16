d39805a71001df8cb3c35a57d824b9500c47933d
pdao1 <69880265+pdao1@users.noreply.github.com>
2022-10-16 04:24:01 -1000

    init

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={{ color:'#888', fontSize: 24 }}>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
